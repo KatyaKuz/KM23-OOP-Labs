eb43bb01054d4428c251982f7a9757b0f3c6c6ac
Katya Kuznetsova <katya043gp@gmail.com>
2023-12-08 21:21:29 +0200

    Lab2 matrix

--- Lab2/src/main/java/Lab2.java
+++ Lab2/src/main/java/Lab2.java
@@ -1,5 +1,88 @@
+import java.util.Arrays;
+
 public class Lab2 {
     public static void main(String[] args) {
+        Matrix mt1, mt2, mt3, mt4, mtr;
+        MatrixImmutable mti1, mti2, mti3, mti4, mtir;
+        double [][] t1= new double[][] {{1.1, 1.2, 1.3}, {2.1, 2.2, 2.3}, {3.1, 3.2, 3.3}, {4.1, 4.2, 4.3}};
+        double [][] t2= new double[][] {{110, 120, 130}, {210, 220, 230}, {310, 320, 330}, {410, 420, 430}};
+        double [][] t3=new double[][] {{2, 5, 7}, {6, 3, 4}, {5, -2, -3}};
+        double [][] t4=new double[][] {{11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34}};
+        mt1= new Matrix(t1);
+        mt2=new Matrix(t2);
+        mt3=new Matrix(t3);
+        mt4=new Matrix(t4);
+        mti1= new MatrixImmutable(t1);
+        mti2=new MatrixImmutable(t2);
+        mti3=new MatrixImmutable(t3);
+        mti4=new MatrixImmutable(t4);
+        System.out.println();
 
+        //Розмірність матриці
+        System.out.println("Розмірність матриці mt1=: "+Arrays.toString(mt1.size_matrix()));
+
+        //Перевірка суми
+        mtr= mt1.sum(mt2);
+        print_array(mtr.get_table());
+        mtir= mti1.sum(mti2);
+        print_array(mtir.get_table());
+
+        //Перевірка множення на скаляр
+        mtr=mt1.multiplication(2);
+        print_array(mtr.get_table());
+        mtir= mti1.multiplication(2);
+        print_array(mtir.get_table());
+
+        //Перевірка множення на матрицю
+        mtr=mt1.multiplication(mt4);
+        print_array(mtr.get_table());
+        mtir= mti1.multiplication(mti4);
+        print_array(mtir.get_table());
+
+        //Перевірка транспонування матриці
+        mtr=mt1.transpose();
+        print_array(mtr.get_table());
+        mtir= mti1.transpose();
+        print_array(mtir.get_table());
+
+        //Перевірка діагональної матриці(вектор - масив)
+        mtr=mt1.diagonal(new double [] {11, 22, 33, 44});
+        print_array(mtr.get_table());
+        mtir= mti1.diagonal(new double [] {11, 22, 33, 44});
+        print_array(mtir.get_table());
+
+        //Перевірка діагональної матриці(вектор - матриця)
+        mtr=mt1.diagonal(new Matrix (new double[][] {{11}, {22}, {33}, {44}}));
+        print_array(mtr.get_table());
+        mtir= mti1.diagonal(new MatrixImmutable (new double[][] {{11}, {22}, {33}, {44}}));
+        print_array(mtir.get_table());
+
+        //Перевірка одиничної матриці
+        mtr=mt1.identity(4);
+        print_array(mtr.get_table());
+        mtir= mti1.identity(4);
+        print_array(mtir.get_table());
+
+        //Перевірка оберненої матриці
+        mtr=mt3.inverse(mt3);
+        print_array(mtr.get_table());
+        mtir= mti3.inverse(mti3);
+        print_array(mtir.get_table());
+    }
+    private static void print_array(double[][] v){
+        System.out.println("--------------------------");
+        if (v.length>0){
+            for (int i=0; i<v.length; i++){
+                for (int  j=0; j<v[0].length; j++){
+                    if (v[i][j]==0){
+                        System.out.printf("%12.2f", 0.00);
+                    }
+                    else{
+                        System.out.printf("%12.2f", v[i][j]);
+                    }
+                }
+                System.out.println("");
+            }
+        }
     }
 }
---
+++ Lab2/src/test/java/MatrixTest.java
@@ -0,0 +1,356 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+    double[][] t;
+    Matrix mt;
+
+    @BeforeEach
+    void setUp() {
+        t=new double[][] {{11, 12, 13}, {21, 22, 23}, {31, 32, 33}, {41, 42, 43}};
+        mt=new Matrix(t);
+
+    }
+
+    @Test
+    void rows_count() {
+        Assertions.assertEquals(4, mt.rows_count());
+        Assertions.assertNotEquals(3, mt.rows_count());
+    }
+
+    @Test
+    void cols_count() {
+        Assertions.assertEquals(3, mt.cols_count());
+        Assertions.assertNotEquals(4, mt.cols_count());
+    }
+
+    @Test
+    void size_matrix() {
+        Assertions.assertArrayEquals(new int[] {4,3}, mt.size_matrix());
+    }
+
+    @Test
+    void check_rc() {
+        Assertions.assertTrue(mt.check_rc(0, 0, false));
+        Assertions.assertTrue(mt.check_rc(3, 2, false));
+        Assertions.assertFalse(mt.check_rc(4, 2, false));
+        Assertions.assertFalse(mt.check_rc(1, 3, false));
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.check_rc(4, 2, true);
+        });
+        Assertions.assertEquals("Номер рядка перевищує розмір матриці.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.check_rc(1, 3, true);
+        });
+        Assertions.assertEquals("Номер стовпчика перевищує розмір матриці.",thrown2.getMessage());
+    }
+
+    @Test
+    void set_element() {
+        mt.set_element(1, 1, 11.1);
+        Assertions.assertArrayEquals(new double[][] {{11, 12, 13}, {21, 11.1, 23}, {31, 32, 33}, {41, 42, 43}}, mt.get_table());
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_element(4, 1, 41);
+        });
+        Assertions.assertEquals("Номер рядка перевищує розмір матриці.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_element(1, 3, 41);
+        });
+        Assertions.assertEquals("Номер стовпчика перевищує розмір матриці.",thrown2.getMessage());
+        IllegalArgumentException thrown3 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_element(1, -1, 41);
+        });
+        Assertions.assertEquals("Номер стовпчика меньше нуля.",thrown3.getMessage());
+        IllegalArgumentException thrown4 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_element(-1, 1, 41);
+        });
+        Assertions.assertEquals("Номер рядка меньше нуля.",thrown4.getMessage());
+    }
+
+    @Test
+    void set_row() {
+        mt.set_row(1, new double[]{21.1, 22.1, 23.1});
+        Assertions.assertArrayEquals(new double[][] {{11, 12, 13}, {21.1, 22.1, 23.1}, {31, 32, 33}, {41, 42, 43}}, mt.get_table());
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_row(4, new double[]{21.1, 22.1, 23.1});
+        });
+        Assertions.assertEquals("Номер рядка перевищує розмір матриці.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_row(3, new double[]{21.1, 22.1, 23.1, 24.1});
+        });
+        Assertions.assertEquals("Кількість стовпчиків повина дорівнювати кількості значень.",thrown2.getMessage());
+        IllegalArgumentException thrown3 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_row(3, new double[]{21.1, 22.1});
+        });
+        Assertions.assertEquals("Кількість стовпчиків повина дорівнювати кількості значень.",thrown3.getMessage());
+        IllegalArgumentException thrown4 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_row(-1, new double[]{21.1, 22.1, 23.1});
+        });
+        Assertions.assertEquals("Номер рядка меньше нуля.",thrown4.getMessage());
+    }
+
+    @Test
+    void set_col() {
+        mt.set_col(1, new double[]{12.1, 22.1, 32.1, 42.1});
+        Assertions.assertArrayEquals(new double[][] {{11, 12.1, 13}, {21, 22.1, 23}, {31, 32.1, 33}, {41, 42.1, 43}}, mt.get_table());
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_col(3, new double[]{12.1, 22.1, 32.1, 42.1});
+        });
+        Assertions.assertEquals("Номер стовпчика перевищує розмір матриці.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_col(2, new double[]{12.1, 22.1, 32.1, 42.1, 52.1});
+        });
+        Assertions.assertEquals("Кількість рядків повина дорівнювати кількості значень.",thrown2.getMessage());
+        IllegalArgumentException thrown3 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_col(2, new double[]{21.1, 22.1});
+        });
+        Assertions.assertEquals("Кількість рядків повина дорівнювати кількості значень.",thrown3.getMessage());
+        IllegalArgumentException thrown4 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_col(-1, new double[]{12.1, 22.1, 32.1, 42.1});
+        });
+        Assertions.assertEquals("Номер стовпчика меньше нуля.",thrown4.getMessage());
+    }
+
+    @Test
+    void set_table() {
+        mt.set_table(new double[][] {{111, 112, 113}, {121, 122, 123}, {131, 132, 133}, {141, 142, 143}});
+        Assertions.assertArrayEquals(new double[][] {{111, 112, 113}, {121, 122, 123}, {131, 132, 133}, {141, 142, 143}}, mt.get_table());
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_table(new double[][] {{111, 112, 113, 114}, {121, 122, 123, 124}, {131, 132, 133, 134}, {141, 142, 143, 144}});
+        });
+        Assertions.assertEquals("Кількість стовпчиків повина дорівнювати кількості стовпчиків значень.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_table(new double[][] {{111, 112, 113}, {121, 122, 123}, {131, 132, 133}, {141, 142, 143}, {151, 152, 153}});
+        });
+        Assertions.assertEquals("Кількість рядків повина дорівнювати кількості рядків значень.",thrown2.getMessage());
+        IllegalArgumentException thrown3 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_table(new double[][] {{111, 112, 113}, {121, 122, 123}, {131, 132, 133}});
+        });
+        Assertions.assertEquals("Кількість рядків повина дорівнювати кількості рядків значень.",thrown3.getMessage());
+        IllegalArgumentException thrown4 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.set_table(new double[][] {{111, 112}, {121, 122}, {131, 132}, {141, 142}});
+        });
+        Assertions.assertEquals("Кількість стовпчиків повина дорівнювати кількості стовпчиків значень.",thrown4.getMessage());
+    }
+
+    @Test
+    void get_element() {
+        Assertions.assertEquals(22, mt.get_element(1, 1));
+        Assertions.assertNotEquals(22, mt.get_element(0, 0));
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_element(4, 1);
+        });
+        Assertions.assertEquals("Номер рядка перевищує розмір матриці.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_element(1, 3);
+        });
+        Assertions.assertEquals("Номер стовпчика перевищує розмір матриці.",thrown2.getMessage());
+        IllegalArgumentException thrown3 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_element(1, -1);
+        });
+        Assertions.assertEquals("Номер стовпчика меньше нуля.",thrown3.getMessage());
+        IllegalArgumentException thrown4 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_element(-1, 1);
+        });
+        Assertions.assertEquals("Номер рядка меньше нуля.",thrown4.getMessage());
+    }
+
+    @Test
+    void get_row() {
+        Assertions.assertArrayEquals(new double []{21, 22, 23}, mt.get_row(1));
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_row(4);
+        });
+        Assertions.assertEquals("Номер рядка перевищує розмір матриці.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_row(-1);
+        });
+        Assertions.assertEquals("Номер рядка меньше нуля.",thrown2.getMessage());
+    }
+
+    @Test
+    void get_col() {
+        Assertions.assertArrayEquals(new double []{12, 22, 32, 42}, mt.get_col(1));
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_col(3);
+        });
+        Assertions.assertEquals("Номер стовпчика перевищує розмір матриці.",thrown1.getMessage());
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.get_col(-1);
+        });
+        Assertions.assertEquals("Номер стовпчика меньше нуля.",thrown2.getMessage());
+    }
+
+    @Test
+    void get_table() {
+        Assertions.assertArrayEquals(new double[][] {{11, 12, 13}, {21, 22, 23}, {31, 32, 33}, {41, 42, 43}}, mt.get_table());
+    }
+
+    @Test
+    void testEquals() {
+        double[][] t1, t2;
+        Matrix mt1, mt2;
+        t1=new double[][] {{11, 12, 13}, {21, 22, 23}, {31, 32, 33}, {41, 42, 43}};
+        mt1=new Matrix(t1);
+        t2=new double[][] {{11, 12, 13}, {21, 52, 23}, {31, 32, 33}, {41, 42, 43}};
+        mt2=new Matrix(t2);
+        Assertions.assertTrue(mt.equals(mt1));
+        Assertions.assertFalse(mt.equals(mt2));
+    }
+
+    @Test
+    void testHashCode() {
+        Assertions.assertEquals(-982233452, mt.hashCode());
+        Assertions.assertNotEquals(982233452, mt.hashCode());
+    }
+
+    @Test
+    void sum() {
+        double[][] t1, ts;
+        Matrix mt1, mt2, mts;
+        t1=new double[][] {{111, 112, 113}, {121, 122, 123}, {131, 132, 133}, {141, 142, 143}};
+        mt1=new Matrix(t1);
+        mt2=mt.sum(mt1);
+        ts=new double[][] {{122, 124, 126}, {142, 144, 146}, {162, 164, 166}, {182, 184, 186}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+    }
+
+    @Test
+    void multiplication() {
+        double[][] ts;
+        Matrix mt2, mts;
+        mt2=mt.multiplication(2);
+        ts=new double[][] {{22, 24, 26}, {42, 44, 46}, {62, 64, 66}, {82, 84, 86}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+    }
+
+    @Test
+    void testMultiplication() {
+        double[][] t1,t11, ts;
+        Matrix mt1, mt11, mt2, mts;
+        t1=new double[][] {{111, 112, 113, 114}, {121, 122, 123, 124}, {131, 132, 133, 134}};
+        mt1=new Matrix(t1);
+        mt2=mt.multiplication(mt1);
+        ts=new double[][] {{4376.00,4412.00,4448.00,4484.00},
+                {8006.00,8072.00,8138.00,8204.00},
+                {11636.00,11732.00,11828.00,11924.00},
+                {15266.00,15392.00,15518.00,15644.00}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+        t11=new double[][] {{111, 112, 113}, {121, 122, 123}, {131, 132, 133}, {141, 142, 143}};
+        mt11=new Matrix(t11);
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            mt.multiplication(mt11);
+        });
+        Assertions.assertEquals("Кількість стовпчиків першої матриці повино дорівнювати кількості рядків другої матриці.",thrown1.getMessage());
+    }
+
+    @Test
+    void transpose() {
+        double[][] ts;
+        Matrix mt2, mts;
+        mt2=mt.transpose();
+        ts=new double[][] {{11, 21, 31, 41}, {12, 22, 32, 42}, {13, 23, 33, 43}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+    }
+
+    @Test
+    void diagonal() {
+        double[] v={11,22,33,44,55};
+        double[][] ts;
+        Matrix mt2, mts;
+        mt2=mt.diagonal(v);
+        ts=new double[][] {{11, 0, 0, 0, 0}, {0, 22, 0, 0, 0}, {0, 0, 33, 0, 0}, {0, 0, 0, 44, 0}, {0, 0, 0, 0, 55}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+    }
+
+    @Test
+    void testDiagonal() {
+        double[][] v={{11},{22},{33},{44},{55}};
+        double[][] vh={{11,22,33,44,55}};
+        double[][] ts;
+        Matrix mt2, mts, mtv;
+        mtv=new Matrix(v);
+        mt2=mt.diagonal(mtv);
+        ts=new double[][] {{11, 0, 0, 0, 0}, {0, 22, 0, 0, 0}, {0, 0, 33, 0, 0}, {0, 0, 0, 44, 0}, {0, 0, 0, 0, 55}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+
+        mtv=new Matrix(vh);
+        mt2=mt.diagonal(mtv);
+        ts=new double[][] {{11, 0, 0, 0, 0}, {0, 22, 0, 0, 0}, {0, 0, 33, 0, 0}, {0, 0, 0, 44, 0}, {0, 0, 0, 0, 55}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+    }
+
+    @Test
+    void identity() {
+        double[][] ts;
+        Matrix mt2, mts;
+        mt2=mt.identity(5);
+        ts=new double[][] {{1, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}};
+        mts=new Matrix(ts);
+        Assertions.assertTrue(mt2.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+    }
+
+    @Test
+    void random_row() {
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            Matrix mts=mt.random_row(-1);
+        });
+        Assertions.assertEquals("Розмірність матриці повина бутит більше нуля.",thrown1.getMessage());
+        Matrix mts1=mt.random_row(3);
+        Assertions.assertNotEquals(mts1.get_element(0,0), mts1.get_element(0,1));
+    }
+
+    @Test
+    void random_col() {
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            Matrix mts=mt.random_col(-1);
+        });
+        Assertions.assertEquals("Розмірність матриці повина бутит більше нуля.",thrown1.getMessage());
+        Matrix mts1=mt.random_col(3);
+        Assertions.assertNotEquals(mts1.get_element(0,0), mts1.get_element(1,0));
+    }
+
+    @Test
+    void inverse() {
+        double[][] t1, ti;
+        Matrix mt1, mti, mts, mts1, mts1s;
+        t1=new double[][] {{2, 5, 7}, {6, 3, 4}, {5, -2, -3}};
+        mt1=new Matrix(t1);
+        mts=mt1.inverse(mt1);
+        ti=new double[][] {{1, -1, 1}, {-38, 41, -34}, {27, -29, 24}};
+        mti=new Matrix(ti);
+        Assertions.assertTrue(mti.equals(mts));
+        Assertions.assertFalse(mt.equals(mts));
+        mts1=mt1.multiplication(mti);
+        mts1s=new Matrix(new double[][] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+        Assertions.assertTrue(mts1.equals(mts1s));
+        IllegalArgumentException thrown1 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            Matrix mt_sn=mt.inverse(mt);
+        });
+        Assertions.assertEquals("Кількість рядків матриці повина дорівнювати кількості стовпчиків.",thrown1.getMessage());
+        Matrix mt4=new Matrix(new double[][] {{11, 12, 13}, {21, 22, 23}, {31, 32, 33}});
+        IllegalArgumentException thrown2 = Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            Matrix mt_sn=mt4.inverse(mt4);
+        });
+        Assertions.assertEquals("Матриця вироджена, тому не може бути обернена.",thrown2.getMessage());
+    }
+
+}
